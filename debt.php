360902ff9d2d84517b726ca7b881ec0aab73fb58
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-10-06 04:45:33 +0100

    view all debtor

--- debt.php
+++ debt.php
@@ -1,8 +1,7 @@
 <?php
 include("functions/top.php");
-$data = $_GET['id'];
-$cls  = $_GET['cls'];
-$trm  = $_GET['trm'];
+$data = $_SESSION['trm'];
+$ses   = $_SESSION['aca'];
 ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -11,12 +10,12 @@ $trm  = $_GET['trm'];
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1><?php echo $data ?> Fee Record(s)</h1>
+                    <h1>View All Debtors</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="./">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Payment History</li>
+                        <li class="breadcrumb-item active">Debtors</li>
                     </ol>
                 </div>
             </div>
@@ -42,38 +41,122 @@ $trm  = $_GET['trm'];
                                     data-card-widget="collapse"><i class="fas fa-minus"></i>
                                 </button>
                             </div>
-                            <h3 class="card-title"><?php echo $trm." - ".$_SESSION['aca']; ?> Academic Session</h3>
+                            <h3 class="card-title"><?php echo $_SESSION['trm']." - ".$_SESSION['aca']; ?> Academic
+                                Session</h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
                             <table id="example1" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
-                                        <th>Amount Paid</th>
-                                        <th>Payment Description</th>
-                                        <th>Mode of Payment</th>
-                                        <th>Payment Date</th>
+                                        <th>Admission No.</th>
+                                        <th>Name</th>
+                                        <th>Class</th>
+                                        <th>Fee</th>
+                                        <th>Paid</th>
+                                        <th>Balance</th>
+                                        <th>Spill Over</th>
+                                        <th>Total Debt</th>
                                         <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php
-                  $der = $_SESSION['aca'];
 
-                  $sql = "SELECT * FROM feercrd WHERE `session` = '$der' AND `adid` = '$data' AND `class` = '$cls' ORDER BY `id` desc";
-                  $rsl = query($sql);
-                  while($row = mysqli_fetch_array($rsl)) 
-                  {
-                  ?>
                                     <tr>
-                                        <td class="font-weight-bolder text-danger">
-                                            ₦<?php echo number_format($row['amount']) ?></td>
-                                        <td><?php echo $row['descr'] ?></td>
-                                        <td><?php echo $row['mode'] ?></td>
-                                        <td><?php echo date('l, F d, Y', strtotime($row['datepaid'])); ?></td>
-                                        <td><a href="receipt?id=<?php echo $row['feeid'] ?>">Print Receipt</a></td>
+
+                                        <?php
+                
+                $sql = "SELECT *, sum(`amount`) AS TOTAL FROM `feercrd` WHERE `term` = '$data' AND `session` = '$ses' GROUP BY `name` asc";
+                $result_set=query($sql);
+               
+                while($row= mysqli_fetch_array($result_set)) {
+               
+                  
+                   $name = $row['name'];
+                   $adid = $row['adid'];
+                   $cls  = $row['class'];
+               
+                     
+                   //get spill over fee
+                   $spill = "SELECT sum(`amount`) as spilltot FROM spillover WHERE `adid` = '$adid' AND `name` = '$name'";
+                   $spls  = query($spill);
+                   $sph   = mysqli_fetch_array($spls);
+               
+               
+                   //get spill paid
+                   $sgt ="SELECT sum(`amount`) as spillpay FROM feercrd WHERE `descr` = 'SpillOver Payment' AND `adid` = '$adid'";
+                   $sgl = query($sgt);
+                   $sgh = mysqli_fetch_array($sgl);
+               
+               
+                   
+               
+                   if($data == '1st Term') {
+                         
+                     $ssl ="SELECT sum(fst) as totas from `student` WHERE `class` = '$cls' AND `session` = '$ses' AND `adid` = '$adid'";
+                     $result = query($ssl);
+                     $vfh = mysqli_fetch_array($result);
+                   
+                   } else {
+                   
+                   if($data == '2nd Term'){
+                   
+                     $ssl ="SELECT sum(snd) as totas from `student` WHERE `class` = '$cls' AND `session` = '$ses' AND `adid` = '$adid'";
+                     $result = query($ssl);
+                     $vfh = mysqli_fetch_array($result);
+                   
+                   }else {
+                   
+                   if($data == '3rd Term') {
+                   
+                   
+                     $ssl = "SELECT sum(trd) as totas from `student` WHERE `class` = '$cls' AND `session` = '$ses' AND `adid` = '$adid'";
+                     $result = query($ssl);
+                     $vfh = mysqli_fetch_array($result);
+                   
+                   }
+                   }
+                   }
+               
+               
+                   //spillover total
+                   $spillover = $sph['spilltot'] - $sgh['spillpay'];
+               
+                   //balance total
+                   $ball = $vfh['totas'] - $row['TOTAL'];
+               
+                   if($ball == 0){
+                       
+               
+                   } else {
+               
+                   //spillover + balance
+                   $debt = $ball + $spillover
+                         ?>
+                                        <td><?php echo $row['adid']; ?></td>
+                                        <td><?php echo ucwords($row['name']); ?></td>
+                                        <td><?php echo ucwords($cls); ?></td>
+                                        <td>₦<?php echo number_format($vfh['totas']); ?></td>
+                                        <td>₦<?php echo number_format($row['TOTAL']); ?></td>
+                                        <td class="text-danger font-weight-bolder">
+                                            ₦<?php echo number_format($ball); ?></td>
+                                        <td class="text-danger font-weight-bolder">
+                                            ₦<?php echo number_format($spillover); ?></td>
+                                        <td class="text-danger font-weight-bolder">
+                                            ₦<?php echo number_format($debt); ?></td>
+                                        <td><a
+                                                href="./history?id=<?php echo $row['adid'] ?>&cls=<?php echo $cls ?>&trm=<?php echo $data ?>">
+                                                View Payment
+                                                History</a><br /><br /><a
+                                                href="./spillhistory?id=<?php echo $row['name'] ?>&adid=<?php echo $row['adid'] ?>">
+                                                View Spill Over
+                                                History</a></td>
+                                        </td>
+                                        <td></td>
+                                        </td>
                                     </tr>
                                     <?php
+              }
               }
               ?>
                                 </tbody>
