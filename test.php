fea9b763c059766bb07ffed8786f6d4d88b6b3ff
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2022-02-15 19:03:01 +0100

    test upd

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ include("functions/init.php");
 # Name of column containing names = full_name
 # Simply change the table and column name to what corresponds with your dataset
 
-/*$sql = "SELECT * FROM feercrd";
+$sql = "SELECT * FROM feercrd";
 $res = query($sql);
 while($row = mysqli_fetch_array($res)) {
 
@@ -23,9 +23,8 @@ $sel = "UPDATE feercrd SET `fname` = '$output' WHERE `feeid` = '$id'";
 $rel = query($sel);
 echo confirm($rel);
 
-}*/
+}
 
-$sql = "SELECT sum("
 
 //family name
 //get total pay of family
